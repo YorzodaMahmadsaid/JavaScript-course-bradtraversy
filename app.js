93bf17ece1a65f3f39addb7aa5bf2cfd6b32d8cf
Mahmadsaid <mahmadsaidsayfullo@gmail.com>
2019-12-23 18:31:50 +0500

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-// alert('hello world');
-// console.log('hello world');
+// Мы рассмотрели 4 функции браузера для взаимодействия с пользователем
+
+// alert();
+// prompt();
+// confirm();
+// console.log();
+
+// alert('Ассалому алейкум');
+
+// var age = prompt('Ту чанд солаи?');
+// console.log('Ту', age, 'солаи!');
+
+// var isBoss = confirm("Букаш худути?");
+// if(isBoss == true) {
+//     alert("Ту букка будай");
+// } else{
+//     alert("Ту букка набудай");
+// }
+
+// console.log('Ассалому алейкум');
+
 // console.log(document);
 // console.log(document.querySelector('h1'));
-document.querySelector('h1').style.color = 'red';
+// document.querySelector('h1').style.color = 'red';
+// document.querySelector('h1').style.fontSize = '14px';
+// document.querySelector('h1').style.fontFamily = 'Roboto';
+// document.querySelector('h1').style.fontWeight = '500';
+// document.querySelector('h1').style.paddingTop = '10px';
+// document.querySelector('h1').style.marginBottom = '15px';
+// document.querySelector('h1').style.textAlign = 'center';
