fc1a3db44629c0a5f47b0badd5be346d1f5f10ba
Mahmadsaid <mahmadsaidsayfullo@gmail.com>
2019-12-24 21:28:48 +0500

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// Мы рассмотрели 3 функции браузера для взаимодействия с пользователем это
+// // Мы рассмотрели 3 функции браузера для взаимодействия с пользователем это
 // alert();
 // prompt();
 // confirm();
 
-// И один функция для взаимодействия с разрабодчиком это
+// // И один функция для взаимодействия с разрабодчиком это
 // console.log();
 
 // alert('Ассалому алейкум');
@@ -59,6 +59,24 @@
 // name = 'Navruzbek';
 // console.log(name);
 
+// function varTest() {
+//     var x = 1;
+//     if (true) {
+//       var x = 2;  // та же переменная!
+//       console.log(x);  // 2
+//     }
+//     console.log(x);  // 2
+//   }
+  
+//   function letTest() {
+//     let x = 1;
+//     if (true) {
+//       let x = 2;  // другая переменная
+//       console.log(x);  // 2
+//     }
+//     console.log(x);  // 1
+//   }
+
 // let name;
 // name = 'Mahmadsaid';
 // console.log(name);
@@ -83,3 +101,78 @@
 // const numbers = [1, 2, 3, 4, 5];
 // numbers.push(6);
 // console.log(numbers);
+
+// Primitive Types
+// null
+// undefined
+// Boolean
+// Number
+// String
+
+// Reference Types
+// Array
+// Object
+// Function
+
+// console.log(typeof(undefined));
+// console.log(typeof(1));
+// console.log(typeof(true));
+// console.log(typeof("name"));
+// console.log(typeof(alert));
+// console.log(typeof({name: "Mahmadsaid"}));
+
+// let value = 5;
+// console.log(value); // 5
+// console.log(typeof value); // string
+// console.log(value.length); // undefined
+
+// let value;
+// value = String(555);
+// console.log(value); // 555
+// console.log(typeof value); // string
+// console.log(value.length); // 3
+
+// let value;
+// value = String(4 + 4);
+// console.log(value);
+// console.log(typeof value);
+// console.log(value.length);
+
+// let value;
+// value = String(true);
+// console.log(value);
+// console.log(typeof value);
+// console.log(value.length);
+
+// let value;
+// value = String(Date());
+// console.log(value);
+// console.log(typeof value);
+// console.log(value.length);
+
+// let value;
+// value = String([1, 2, 3, 4, 5]);
+// console.log(value);
+// console.log(typeof value);
+// console.log(value.length);
+
+// let value;
+// value = String({name: "Mahmadsaid"});
+// console.log(value);
+// console.log(typeof value);
+// console.log(value.length);
+
+// let value;
+// value = Number('5');
+// console.log(value);
+// console.log(typeof value);
+// console.log(value.toFixed(3));
+
+// let value = parseInt('100');
+// console.log(typeof value);
+// console.log(value);
+
+// const value1 = String(5);
+// const value2 = 4;
+// const sum = value1 + value2;
+// console.log(sum); //54
