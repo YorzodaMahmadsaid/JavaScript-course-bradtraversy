b4b3ffdb7202eb02fa5f00927ac5862e7185274a
Mahmadsaid <mahmadsaidsayfullo@gmail.com>
2020-03-16 17:15:10 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+/* // 02 Chapter: JavaScript language fundamentals
+// 04 Section: Intro file setup
+alert('Hello World!'); */
+
+
+/* // 05 Using the console
+console.log('Hello World');
+
+console.log(123);
+
+console.log(true);
+
+var greeting = 'Hello';
+console.log(greeting);
+
+console.log([1,2,3,4,5]);
+
+console.log({a:1, b:2, c:3, d:4, f:5});
+
+console.table({a:1, b:2, c:3, d:4, f:5});
+
+console.error('This is some error');
+
+console.clear();
+
+console.warn('This is a warning');
+
+console.time('TimeCounter')
+    console.log("Hello");
+    console.log("Hello");
+    console.log("Hello");
+    console.log("Hello");
+    console.log("Hello");
+console.timeEnd('TimeCounter'); */
+
+
+/* //06 Veriables: var let const
+var name = 'Jone Doe';
+console.log(name);
+
+name = 'Stive Smith';
+console.log(name);
+
+var greeting;
+console.log(greeting); // Undefined
+
+greeting = 'Hello';
+console.log(greeting);
+
+// Multi word vars
+var firstName = 'John'; // Camel case
+var FirstName = 'Tom'; // Pascal case 
+var first_name = 'Sara'; // Underscore
+
+const name = "John";
+console.log(name);
+
+name = 'Sara';
+console.log(name); // error
+
+const person = {
+    name: 'John',
+    age: 31,
+    sex: 'male'
+}
+person.name = 'Sara';
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+console.log(numbers); */
+
+
+// 07 Data types in JavaScript 
+
+
+
+
+
