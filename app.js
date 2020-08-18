b2b6dc7f7da0b7d7a24cae0beab2bea9ea5ed5cb
Mahmadsaid <mahmadsaidsayfullo@gmail.com>
2020-08-18 09:10:06 +0500

    initial commit

--- app.js
+++ app.js
@@ -1834,8 +1834,7 @@ console.log(brad);
 console.log(brad.greeting()); */
 
 
-// 048 ES6 CLASSES
-
+/* // 048 ES6 CLASSES
 class Person1 {
     constructor(firstName, lastName, dob) {
         this.firstName = firstName;
@@ -1871,4 +1870,118 @@ console.log(mary.calculateAge());
 mary.getsMerried('Jonon');
 console.log(mary);
 
-console.log(Person1.addNumbers(1, 2));
+console.log(Person1.addNumbers(1, 2)); */
+
+/* // 049 SUB CLASSES 
+class Person1 {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    greeting() {
+        return `Hello there ${this.firstName} ${this.lastName}`;
+    }
+}
+
+class Customer extends Person1 {
+    constructor(firstName, lastName, phone, membership) {
+        super(firstName, lastName);
+
+        this.phone = phone;
+        this.memberdhip = membership;
+    }
+
+    static getMembershipCost() {
+        return 'Hello Mahmadsaid';
+    }
+}
+
+const john = new Customer('John', 'Doe', '333-333-333', 'Standart');
+
+console.log(john.greeting());
+
+console.log(Customer.getMembershipCost()) */
+
+/* // 050 - 053 ADD BOOK TO LIST
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn; 
+}
+
+function UI() {}
+
+UI.prototype.addBookToList = function(book) {
+    const list = document.getElementById('book-list');
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${book.title}<td>
+    <td>${book.author}<td>
+    <td>${book.isbn}<td>
+    <td><a href="#" class="delete">x<td>
+    `;
+
+    list.appendChild(row);
+}
+
+UI.prototype.showAlert = function(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+    setTimeout(function() {
+        document.querySelector('.alert').remove();
+    }, 3000)
+}
+
+UI.prototype.deleteBook = function(target) {
+    if(target.className === 'delete') {
+        target.parentElement.parentElement.remove();
+    }
+}
+
+
+UI.prototype.clearFields = function() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+
+document.getElementById('book-form').addEventListener('submit', function(e) {
+    const title = document.getElementById('title').value,
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value;
+
+    const book = new Book(title, author, isbn);
+    
+    const ui = new UI();
+
+    if(title === '' || author === '' || isbn === '') {
+        ui.showAlert('Please fill in all fields', 'error');
+    } else {
+        ui.addBookToList(book);
+        ui.showAlert('Book added!', 'success');
+        ui.clearFields();
+    }
+
+    e.preventDefault();
+})
+
+document.getElementById('book-list').addEventListener('click', function(e) {
+
+    const ui = new UI();
+    ui.deleteBook(e.target);
+    ui.showAlert('Book removed!', 'success');
+
+
+    e.preventDefault();
+}) */
+
+// 054 CONVERT TO ES6 CLASSES
+
+
+
