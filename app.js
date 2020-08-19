9e5986ed1b13e8ef0c0b09b9ffa74fddfa0182cf
Mahmadsaid <mahmadsaidsayfullo@gmail.com>
2020-08-19 11:56:47 +0500

    060 tutorial

--- app.js
+++ app.js
@@ -2205,6 +2205,42 @@ function loadCustomers(e) {
 } */
 
 // 060 GETTING DATA FROM AN EXTERNAL API
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+    const number = document.getElementById('number').value;
+    
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+    xhr.onload = function() {
+        if(this.status === 200) {
+            const response = JSON.parse(this.responseText);
+            
+            let output = '';
+
+            if(response.type === 'success') {
+                 response.value.forEach(function(joke) {
+                    output += `<li>${joke.joke}</li>`
+                 });
+            } else {
+                output += '<li>Somthing went wrong</li>';
+            }
+
+            document.querySelector('.jokes').innerHTML = output;
+        }
+
+    }
+
+
+    xhr.send();
+
+    e.preventDefault();
+}
+
+
+// 061 REST APIs HTTP REQUESTS
 
 
 
