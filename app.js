01b7fc85431c28db8fd71da52218a4ef18847d07
Mahmadsaid <mahmadsaidsayfullo@gmail.com>
2020-08-25 13:08:02 +0500

    initial commit

--- app.js
+++ app.js
@@ -2616,6 +2616,112 @@ http.delete('http://jsonplaceholder.typicode.com/users/2', data)
 
 // 69 ASYNC AWAIT
 
+/*async function myFunction() {
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('Hello Mahmadsaid'), 2000);
+    });
+
+    const error = true;
+
+    if(!error) {
+        const res = await promise;
+        return res;
+    } else {
+        await Promise.reject(new Error('Something went wrong'));
+    }
+
+
+}
+
+myFunction()
+    .then(res => console.log(res))
+    .catch(err => console.log(err));*/
+
+/*async function getUsers() {
+    const response = await fetch('http://jsonplaceholder.typicode.com/users');
+
+    const data = await response.json();
+
+    return data;
+}
+
+getUsers().then(users => console.log(users));*/
+
+/*// 70 CUSTOM HTTP LIBRARY, FETCH WITH ASYNC AWAIT - PART 4
+
+class EasyHTTP {
+
+   async get(url) {
+      const response = await fetch(url);
+      const resData = await response.json();
+      return resData;
+    }
+
+   async post(url, data) {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+       const resData = await response.json();
+       return resData;
+    }
+
+   async put(url, data) {
+           const response = await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+       const resData = await response.json();
+       return resData;
+    }
+
+   async delete(url, data) {
+           const response = await fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+       const resData = await 'Response deleted...';
+       return resData;
+    }
+}
+
+const http = new EasyHTTP;
+
+const data = {
+    name: 'Mahmadsaid',
+    username: 'Yorzoda',
+    email: 'said@mail.com'
+}
+
+http.get('http://jsonplaceholder.typicode.com/users', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+http.post('http://jsonplaceholder.typicode.com/users', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+http.put('http://jsonplaceholder.typicode.com/users/2', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+http.delete('http://jsonplaceholder.typicode.com/users/2', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));*/
+
+// 08 API PROJECT
+// 71 GITHUB FINDER PART 1 - INTRO UI
+
 
 
 
